767a04c6396349c5db09f7ad3556cf1491a9603b
Javid Ahmed <57677851+javidahmed64592@users.noreply.github.com>
2026-04-15 19:39:12 +0200

    Initial commit

---
+++ python_template_server/middleware/__init__.py
@@ -0,0 +1,6 @@
+"""Middleware module for server."""
+
+from .request_logging_middleware import RequestLoggingMiddleware
+from .security_headers_middleware import SecurityHeadersMiddleware
+
+__all__ = ["RequestLoggingMiddleware", "SecurityHeadersMiddleware"]
