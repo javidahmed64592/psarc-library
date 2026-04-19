cf2b82439b09abadc5da94cfea1736b2c94761dd
Javid Ahmed <javidahmed@icloud.com>
2026-04-19 02:53:36 +0100

    feat: update coverage thresholds in Jest configuration to 20%

--- psarc-library-frontend/jest.config.js
+++ psarc-library-frontend/jest.config.js
@@ -31,10 +31,10 @@ const customJestConfig = {
   coverageReporters: ["text", "lcov", "html", "json"],
   coverageThreshold: {
     global: {
-      branches: 30,
-      functions: 30,
-      lines: 30,
-      statements: 30,
+      branches: 20,
+      functions: 20,
+      lines: 20,
+      statements: 20,
     },
   },
 };
